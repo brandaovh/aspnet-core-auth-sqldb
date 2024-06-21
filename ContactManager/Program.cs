5e453e154bf4ee7674e3e47b2e67eaa776d9efe5
vh-brandao <157319937+vh-brandao@users.noreply.github.com>
2024-06-21 13:59:41 -0400

    Setting up connection string

--- ContactManager/Program.cs
+++ ContactManager/Program.cs
@@ -5,7 +5,8 @@ using ContactManager.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") ?? throw new InvalidOperationException("Environment variable 'DB_CONNECTION_STRING' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
