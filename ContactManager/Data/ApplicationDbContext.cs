ed8323c76dbd738cca2b0762670cbafef010a158
vh-brandao <157319937+vh-brandao@users.noreply.github.com>
2024-06-21 20:19:30 -0400

    Updated SeedData

--- ContactManager/Data/ApplicationDbContext.cs
+++ ContactManager/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ContactManager.Models;
 
 namespace ContactManager.Data;
 
@@ -9,4 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+public DbSet<ContactManager.Models.Contact> Contact { get; set; } = default!;
 }
--- ContactManager/Program.cs
+++ ContactManager/Program.cs
@@ -17,6 +17,13 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    await SeedData.Initialize(services);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
